57d67790ff18eeee009d679e95fd4ab8e3aaebbd
AhmedEhab2022 <am8122004@gmail.com>
2024-02-04 14:28:39 -0800

    Add handler for 404 errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,7 @@
 Module that contain the Main App
 """
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -19,6 +19,14 @@ app.register_blueprint(app_views)
 def teadown():
     """ Call in this method storage.close() to remove db session"""
     storage.close()
+
+
+@app.errorhandler(404)
+def error404(e):
+    """Return a JSON-formatted 404 status code response"""
+    response = jsonify({"error": "Not found"})
+    response.status_code = 404
+    return response
 
 
 if __name__ == "__main__":
